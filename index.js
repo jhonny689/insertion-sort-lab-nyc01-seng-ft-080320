d43e77db6d289f9889b7bc8f8cd56533f188352e
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-12-24 05:33:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function findMinAndRemove(array){
-
+  let min = array[0],
+      minIndex = 0;
+  for(let i = 1; i < array.length; i++){
+    if(array[i] < min){
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex,1);
+  return min;
 }
 
 function selectionSort(array){
